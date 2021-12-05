6a491c24be313cffbeaa33d79e7908d709404e98
mosesmbugua <swahili1963@gmail.com>
2021-12-05 15:34:37 -0800

    SMS Notifications

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # react-chat-app
 
 This application used the getStream.io API for creating a chat application
+
+### Twilio
+
+The App uses the twilio API for sending SMS notfications
--- server/index.js
+++ server/index.js
@@ -7,6 +7,20 @@ const app = express();
 const authenticationRoutes = require("./routes/authentication.js");
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const client = require("twilio")(accountSid, authToken);
+
+// client.messages
+//   .create({
+//     body: "Bro what is happening to me",
+//     messagingServiceSid: messagingServiceSid,
+//     to: "+14252691572",
+//   })
+//   .then((message) => console.log(message.sid))
+//   .done();
 var colors = require("colors");
 
 app.use(cors());
@@ -15,6 +29,34 @@ app.use(express.urlencoded());
 
 app.get("/", (request, response) => {
   response.send("Hello bitches");
+});
+
+//send sms notifications to users
+app.post("/", (request, response) => {
+  const { message, user: sender, type, members } = request.body;
+
+  console.log("anybody there");
+
+  if (type === "message.new") {
+    console.log("anybody there");
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        // if (!user.online) {
+        client.messages
+          .create({
+            body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+            messagingServiceSid: messagingServiceSid,
+            to: "+14252691572",
+          })
+          .then(() => console.log("Message sent!"))
+          .catch((err) => console.log(err));
+        // }
+      });
+    return response.status(200).send("Message sent!");
+  }
+
+  return response.status(200).send("Not a new message request");
 });
 
 app.use("/auth", authenticationRoutes);
